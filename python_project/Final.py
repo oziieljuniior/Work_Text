a52c2f4479e673603bb5f293f17fc607546df4a5
Oziel Junior <ozieljr14@gmail.com>
2023-02-03 20:58:15 -0400

    att

--- python_project/Final.py
+++ python_project/Final.py
@@ -99,7 +99,7 @@ for caminho in lista_caminho:
             
             #configurando texto txt
             path_texto_original = path_texto +"/"+ titulo
-            texto_txt = open(path_texto, 'x', encoding = 'utf-8')
+            texto_txt = open(path_texto_original, 'x', encoding = 'utf-8')
             
             #Configuração para extração do texto
             path2 = path1 + "/" + caminho1
@@ -114,7 +114,7 @@ for caminho in lista_caminho:
                     interpreter.process_page(page)
             #variavel de retorno da extração do texto
             text = output_string.getvalue()
-            texto_txt.wrtie(text)
+            texto_txt.write(text)
             print("Extração de texto completa!")
             texto_txt.close()
             
@@ -135,6 +135,7 @@ for caminho in lista_caminho:
                 if matches[i].ruleId != 'WHITESPACE_RULE' and matches[i].ruleId != 'DASH_RULE' and matches[i].ruleId != 'HUNSPELL_RULE' and matches[i].ruleId != 'SPACE_BEFORE_PUNCTUATION' and matches[i].ruleId != 'ORDINAL_ABREVIATION' and matches[i].ruleId != 'SENT_START_NUM' and matches[i].ruleId != 'ROMAN_NUMBERS_CHECKER' and matches[i].ruleId != 'DECIMAL_COMMA' and matches[i].ruleId != 'PT_COMPOUNDS_POST_REFORM' and matches[i].ruleId != 'UNPAIRED_BRACKETS' and matches[i].ruleId != 'PT_BARBARISMS_REPLACE' and matches[i].ruleId != 'PT_BR_SIMPLE_REPLACE' and matches[i].ruleId != 'CHEMICAL_FORMULAS_TYPOGRAPHY' and matches[i].ruleId != 'GENERAL_NUMBER_FORMAT' and matches[i].ruleId != 'GENERAL_NUMBER_AGREEMENT_ERRORS'and matches[i].ruleId != 'PHRASE_REPETITION' and  matches[i].ruleId !='ABREVIATIONS_PUNCTUATION' and matches[i].ruleId != 'COPYRIGHT' and matches[i].ruleId != 'BARBARISMS' and matches[i].ruleId != 'UPPERCASE_AFTER_COMMA' and matches[i].ruleId != 'GENERAL_GENDER_AGREEMENT_ERRORS' and matches[i].ruleId != 'ZERO_IN_DAYS_OF_THE_MONTH' and matches[i].ruleId != 'PORTUGUESE_WORD_REPEAT_RULE' and matches[i].ruleId != 'GENTILICOS_LINGUAS' and matches[i].ruleId != 'LOWERCASE_RARE_WORDS_GEOGRAPHICAL' and matches[i].ruleId != 'UPPERCASE_SENTENCE_START' and matches[i].ruleId != 'REPEATED_WORDS_3X' and matches[i].ruleId != 'SI_UNITS_CASE' and matches[i].ruleId != 'PERCENT_WITHOUT_SPACE' and matches[i].ruleId != 'ARTICLES_PRECEDING_LOCATIONS' and matches[i].ruleId != 'UNPAIRED_BRACKET_SUGGESTIONS' and matches[i].ruleId != 'COMMA_PARENTHESIS_WHITESPACE' and matches[i].ruleId != 'DOUBLE_PUNCTUATION' and matches[i].ruleId != 'A_WORD' and matches[i].ruleId != 'HOMONYM_ASSO_10' and matches[i].ruleId != 'AO90_MONTHS_CASING' and matches[i].ruleId != 'ABREVIATIONS_PUNCTUATION' and matches[i].ruleId != 'NO_SPACE_CLOSING_QUOTE' and matches[i].ruleId != 'CERCA_DE_NR' and matches[i].ruleId != 'NUMBER_ABREVIATION' and matches[i].ruleId != 'INFORMALITIES' and matches[i].ruleId != 'ENUMERATIONS_AND_AND' and matches[i].ruleId != 'ARCHAISMS' and matches[i].ruleId != 'INVALID_DATE' and matches[i].ruleId != 'PT_CLICHE_REPLACE' and matches[i].ruleId != 'QUE_SER_ESTAR_PARTPASSADO' and matches[i].ruleId != 'ALTERNATIVE_CONJUNCTIONS_COMMA' and matches[i].ruleId != 'E_É_SÃO_FOI_FORAM_SENDO_SIDO' and matches[i].ruleId != 'QUE_FORAM_FOI_SÃO_É_SENDO' and matches[i].ruleId != 'INTERNET_ABBREVIATIONS' and matches[i].ruleId != 'PORTUGUESE_WORD_REPEAT_BEGINNING' and matches[i].ruleId != 'INTERJECTIONS_PUNTUATION' and matches[i].ruleId !='ERRO_DE_CONCORDNCIA_DO_GÉNERO_MASCULINO_O' and matches[i].ruleId != 'FRAGMENT_TWO_PREPOSTIONS' and matches[i].ruleId != 'ADVERBIOS_MODO_EM_SEQUENCIA' and matches[i].ruleId != 'CUJO_LIGACAO_NOME_ADJETIVO_NUMERAL' and matches[i].ruleId != 'PP_OBJ_IND' and matches[i].ruleId != 'PT_SIMPLE_REPLACE' and matches[i].ruleId != 'REDUNDANT_CONJUNCTIONS' and matches[i].ruleId != 'COLOCAÇÃO_ADVÉRBIO' and matches[i].ruleId != 'INTERROGATIVES_PUNTUATION' and matches[i].ruleId != 'YEAR_NUMBER_FORMAT' and matches[i].ruleId != 'VERB_COMMA_CONJUNCTION' and matches[i].ruleId != 'REDUNDANCY_JUNTO_COM' and matches[i].ruleId != 'PORTUGUESE_WRONG_WORD_IN_CONTEXT' and matches[i].ruleId != 'PT_WEASELWORD_REPLACE' and matches[i].ruleId != 'COLOCACAO_ADVERBIOS_LUGAR' and matches[i].ruleId != 'SENTENCE_WHITESPACE' and matches[i].ruleId != 'DATE_NEW_YEAR' and matches[i].ruleId != 'kWh' and matches[i].ruleId != 'ALÉM_AQUÉM_RECÉM' and matches[i].ruleId != 'PROFANITY' and matches[i].ruleId != 'PARENTESESE_AND_QUOTES_SPACING' and matches[i].ruleId != 'EM_LONGO_PRAZO' and matches[i].ruleId != 'SUBSTANTIVO_PLURAL_E_CHAVE':
                     erro_local = erro_local + 1
                     erro_anual = erro_anual + 1
+                    aperro.write("Erro: ")
                     aperro.write(str(erro_local))
                     aperro.write('\n')
                     aperro.write(str(matches[i]))
@@ -152,7 +153,9 @@ for caminho in lista_caminho:
             
             contador += 1
         
-        excel3.save(path_resultado + "/" + caminho_pdf + ".xlsx")
+        name = path_resultado + "/" + caminho + ".xlsx"
+        print("Salvo: ", name)
+        excel3.save(name)
         
         indice_excel += 1
         indice_planilhaA = 'A' + str(indice_excel)
@@ -184,7 +187,7 @@ for caminho in lista_caminho:
         contador_geral += 1
     
     print("Realizando ajuste de pasta: ", path1)
-    path_saida = path_saida_original + caminho
+    path_saida = path_saida_original + "/" + caminho
     print("Para: ", path_saida)
     os.rename(path1, path_saida)
 
