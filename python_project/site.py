3729719a5c9157e6109c0650294b41e5b29ad63b
Oziel Junior <ozieljr14@gmail.com>
2023-03-15 17:55:24 -0400

    att

--- python_project/site.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pyautogui
-
-#C:/Users/M&A SOLUTIONS BRASIL/Documents/Att/Cias não financeiras - 12-12-2022.xlsx
-data = pd.read_excel('C:/Users/M&A SOLUTIONS BRASIL/Documents/Att/Cias não financeiras - 12-12-2022.xlsx')
-consulta = data['ticker'].to_list()
-
-
-pyautogui.screenshot().save('aloha.png')
